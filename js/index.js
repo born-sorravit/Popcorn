9380d6219246abc0866ae5e207b8c67019535650
Nutthapon <armnutthapon@hotmail.co.th>
2020-09-30 16:56:04 +0700

    poster image from firebase

---
+++ js/index.js
@@ -0,0 +1,38 @@
+$(function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyDyXjr-BUDZTM5MVKdQvDVMlINCofdTFlo",
+        authDomain: "popcorn-9afb7.firebaseapp.com",
+        databaseURL: "https://popcorn-9afb7.firebaseio.com",
+        projectId: "popcorn-9afb7",
+        storageBucket: "popcorn-9afb7.appspot.com",
+        messagingSenderId: "787336592475",
+        appId: "1:787336592475:web:4b54b4d16cb39e033f537e",
+        measurementId: "G-9JXPL486H7"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    var db = firebase.firestore();
+
+    db.collection("moviePoster").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            var row = `<div class="text-center">
+            <ons-carousel swipeable auto-scroll overscrollable id="carousel"
+                style="text-align: center; background-color: black;">
+                <ons-carousel-item>
+                <img class="card-img-top logo carouselPoster" src="${doc.data().poster1}" alt="">
+                </ons-carousel-item>
+                <ons-carousel-item>
+                <img class="card-img-top logo carouselPoster" src="${doc.data().poster2}" alt="">                
+                </ons-carousel-item>
+                <ons-carousel-item>
+                <img class="card-img-top logo carouselPoster" src="${doc.data().poster3}" alt="">
+                </ons-carousel-item>
+            </ons-carousel>
+            </div>`;
+            $('#list').append(row);
+        });
+    });
+
+
+})

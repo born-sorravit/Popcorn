09a7b2ed18453357cba6b50a21f35e9e1d47e00e
kannik19 <kannika.k1999@gmail.com>
2020-09-30 15:53:44 +0700

    new

---
+++ js/app.js
@@ -0,0 +1,20 @@
+document.addEventListener('init', function (event) {
+    var page = event.target;
+    console.log(page.id);
+    if (page.id === 'page1') {
+        $('#back').hide();
+        page.querySelector('#bw').onclick = function () {
+            document.querySelector('#myNavigator').pushPage('views/blackwidow.html'); 
+            page.querySelector('#bb').onclick = function () {
+                document.querySelector('#myNavigator').pushPage('views/bw.html');
+            };
+        };
+    } else if (page.id === 'bw' || page.id === 'bb') {        
+        $('#back').show();      
+        document.querySelector('ons-back-button').onclick = function(event) {
+            document.querySelector('#myNavigator').popPage();
+        };
+
+    }
+
+});

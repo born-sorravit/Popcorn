fb3355e1ef8b0cca79bf19bf5e4d701d78a7086e
Nutthapon <armnutthapon@hotmail.co.th>
2020-10-01 16:22:11 +0700

    take a break

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 $(function() {
+
     var firebaseConfig = {
         apiKey: "AIzaSyDyXjr-BUDZTM5MVKdQvDVMlINCofdTFlo",
         authDomain: "popcorn-9afb7.firebaseapp.com",
@@ -20,7 +21,7 @@ $(function() {
             <ons-carousel swipeable auto-scroll overscrollable id="carousel"
                 style="text-align: center; background-color: black;">
                 <ons-carousel-item>
-                <img class="card-img-top logo carouselPoster" src="${doc.data().poster1}" alt="">
+                <img class="card-img-top logo carouselPoster"  src="${doc.data().poster1}" alt="" id="tennet">
                 </ons-carousel-item>
                 <ons-carousel-item>
                 <img class="card-img-top logo carouselPoster" src="${doc.data().poster2}" alt="">                
@@ -106,4 +107,53 @@ $(function() {
         });
     });
 
+
+    // db.collection("tennet").get().then((querySnapshot) => {
+    //     querySnapshot.forEach((doc) => {
+    //         var row = `<div class="text-center">
+
+
+
+    //         <span class="list-item__title titleDetail" src="${doc.data().poster}"></span>
+    //         <span class="list-item__subtitle" src="${doc.data().story}"></span>
+    //         </div>`;
+    //         $('#tennet').append(row);
+    //     });
+    // });
+
+
+    db.collection("tennet").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            var row = `
+        <img class="card-img-top" src="${doc.data().poster}">
+        <div class="card-body">
+            <h4 class="card-title">${doc.data().title}(${doc.data().year})</h4>
+            <p class="card-text">${doc.data().title}(${doc.data().story}</p>
+        </div>
+    </div>`
+            $("#watchtennet").append(row);
+
+        });
+    });
+
 })
+
+
+
+document.addEventListener('init', function(event) {
+    var page = event.target;
+    console.log(page.id);
+    if (page.id === 'page1') {
+        page.querySelector('#tennet').onclick = function() {
+            document.querySelector('#myNavigator').pushPage('/views/blackwidow.html');
+
+        };
+    } else if (page.id === 'bw' || page.id === 'bb') {
+        $('#back').show();
+        document.querySelector('ons-back-button').onclick = function(event) {
+            document.querySelector('#myNavigator').popPage();
+        };
+
+    }
+
+});

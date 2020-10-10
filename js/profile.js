35dea8a47873c9592eafc44ac1069913e830fa43
wattana <endnapadad25@gmail.com>
2020-10-10 19:35:53 +0700

    update_package

--- js/profile.js
+++ js/profile.js
@@ -1,4 +1,5 @@
-
+$(function(){
+    
     document.addEventListener('init', function (event) {
         var page = event.target;
         if (page.id === 'profile') {
@@ -17,7 +18,9 @@
             page.querySelector('#reportprofile').onclick = function() {
                 document.querySelector('#Navigator_profile').pushPage('views/profile/report.html');
             };
-        } 
+        }else if(page.id === "packagePage"){
+            checkPackage()
+        }
         // else if (page.id === 'editp' || page.id === 'packagep') {
         //     $('#back').show();
         //     document.querySelector('ons-back-button').onclick = function(event) {
@@ -27,3 +30,41 @@
         // }
         
     });
+
+})
+
+function checkPackage(){
+    $("#btnPackage input").click(function(){
+        if($(this).attr("checked",true)){
+            if($(this).attr('id') == "packWeek"){
+                $(".Week").css("color","#00FF0B")
+                $('.Week-close').css("color","#ff0000")
+                $(".Stan , .Deluxe , .Premium , .Premium-close , .Stan-close , .Deluxe-close").css("color","#fff")
+                $("#packageDetail").html("WEEK")
+                $("#price").html("39 Baht")
+                $("#days").html("7 Days")
+            }else if($(this).attr('id') == "packStan"){
+                $(".Stan").css("color","#00FF0B")
+                $('.Stan-close').css("color","#ff0000")
+                $(".Week , .Deluxe , .Premium, .Week-close , .Premium-close , .Deluxe-close").css("color","#fff")
+                $("#packageDetail").html("STANDARD")
+                $("#price").html("89 Baht")
+                $("#days").html("30 Days")
+            }else if($(this).attr('id') == "packDeluxe"){
+                $(".Deluxe").css("color","#00FF0B")
+                $('.Deluxe-close').css("color","#ff0000")
+                $(".Stan , .Week , .Premium , .Week-close , .Premium-close , .Stan-close").css("color","#fff")
+                $("#packageDetail").html("DELUXE")
+                $("#price").html("139 Baht")
+                $("#days").html("30 Days")
+            }else if($(this).attr('id') == "packPremium"){
+                $(".Premium").css("color","#00FF0B")
+                $('.Premium-close').css("color","#ff0000")
+                $(".Stan , .Deluxe , .Week , .Week-close , .Stan-close , .Deluxe-close").css("color","#fff")
+                $("#packageDetail").html("PREMIUM")
+                $("#price").html("189 Baht")
+                $("#days").html("30 Days")
+            }
+        }
+    })
+}

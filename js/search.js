75ebbb06ea7a16e3f6595ca7bc43fa158487f694
kannik19 <kannika.k1999@gmail.com>
2020-10-16 14:40:08 +0700

    button fix

--- js/search.js
+++ js/search.js
@@ -1,63 +1,59 @@
-$(function() {
-    var firebaseConfig = {
-        apiKey: "AIzaSyDyXjr-BUDZTM5MVKdQvDVMlINCofdTFlo",
-        authDomain: "popcorn-9afb7.firebaseapp.com",
-        databaseURL: "https://popcorn-9afb7.firebaseio.com",
-        projectId: "popcorn-9afb7",
-        storageBucket: "popcorn-9afb7.appspot.com",
-        messagingSenderId: "787336592475",
-        appId: "1:787336592475:web:4b54b4d16cb39e033f537e",
-        measurementId: "G-9JXPL486H7"
-    };
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
-    firebase.analytics();
-    var db = firebase.firestore();
+var firebaseConfig = {
+    apiKey: "AIzaSyDyXjr-BUDZTM5MVKdQvDVMlINCofdTFlo",
+    authDomain: "popcorn-9afb7.firebaseapp.com",
+    databaseURL: "https://popcorn-9afb7.firebaseio.com",
+    projectId: "popcorn-9afb7",
+    storageBucket: "popcorn-9afb7.appspot.com",
+    messagingSenderId: "787336592475",
+    appId: "1:787336592475:web:4b54b4d16cb39e033f537e",
+    measurementId: "G-9JXPL486H7"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
 
-    db.collection("John Wick").get().then((querySnapshot) => {
+$(function() {
+
+    db.collection("movieDetail").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            var row = `<div class="text-center">
-            <ons-carousel swipeable auto-scroll overscrollable id="carousel"
-                style="text-align: center; background-color: black;">
-                <ons-carousel-item>
-                <img class="card-img-top logo carouselPoster" src="${doc.data().img}" alt="">
-                </ons-carousel-item>
-                <ons-carousel-item>
-                <img class="card-img-top logo carouselPoster" src="${doc.data().title}" alt="">                
-                </ons-carousel-item>
-                <ons-carousel-item>
-                <img class="card-img-top logo carouselPoster" src="${doc.data().year}" alt="">
-                </ons-carousel-item>
-                <ons-carousel-item>
-                <img class="card-img-top logo carouselPoster" src="${doc.data().hour}" alt="">
-                </ons-carousel-item>
-            </ons-carousel>
-            </div>`;
+            var row = `<ons-col onclick="openHome()">
+                <div class="user_thumbnail">
+                                <img src="${doc.data().img}" alt="" class="searchPoster"> 
+                            </div>
+                            
+                </ons-col>
+
+               `;
             $('#list').append(row);
         });
     });
 
-    db.collection("Venom").get().then((querySnapshot) => {
+});
+
+function Src() {
+
+    const result = document.getElementById('Srcname').value;
+    console.log(result);
+
+    $('#list').empty();
+    db.collection("movieDetail").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            var row = `<div class="text-center">
-            <ons-carousel swipeable auto-scroll overscrollable id="carousel"
-            style="text-align: center; background-color: black;" item-width="150px">
-            <ons-carousel-item>
-            <img class="card-img-top logo sizeposter" src="${doc.data().img}" alt="" >
-            </ons-carousel-item>
-            <ons-carousel-item>
-            <img class="card-img-top logo sizeposter" src="${doc.data().title}" alt="" >
-            </ons-carousel-item>
-            <ons-carousel-item>
-            <img class="card-img-top logo sizeposter" src="${doc.data().year}" alt="" >                
-            </ons-carousel-item>
-            <ons-carousel-item>
-            <img class="card-img-top logo sizeposter" src="${doc.data().hour}" alt="" >
-            </ons-carousel-item>
-        </ons-carousel>
-            </div>`;
-            $('#continue').append(row);
+            const titleMovie = doc.data().title;
+
+            if (titleMovie.indexOf(result) != -1) {
+                var row = `<ons-col onclick="openHome()">
+                <div class="user_thumbnail">
+                                <img src="${doc.data().img}" alt="" class="searchPoster"> 
+                            </div>
+                            <div class="username" id="user_2_name">${doc.data().title}</div>
+                </ons-col>
+
+               `;
+                $('#list').append(row);
+            }
+
         });
     });
-    
-})
+
+}

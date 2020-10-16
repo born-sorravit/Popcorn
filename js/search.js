ddd67f38ef2705077735b0ae6f1f5efb8efd84de
kannik19 <kannika.k1999@gmail.com>
2020-10-16 18:25:10 +0700

    button

--- js/search.js
+++ js/search.js
@@ -12,48 +12,81 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 var db = firebase.firestore();
-
+  
 $(function() {
-
     db.collection("movieDetail").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
-            var row = `<ons-col onclick="openHome()">
-                <div class="user_thumbnail">
-                                <img src="${doc.data().img}" alt="" class="searchPoster"> 
+            var row = `<div class="col-4 areaMovie movieF center" id="${doc.data().id}">
+                            <div class="user_thumbnail margin-15">
+                                <img src="${doc.data().img}" alt="" class="searchPoster mt-5"> 
                             </div>
-                            
-                </ons-col>
-
-               `;
-            $('#list').append(row);
+                        </div>
+                `;
+            $('#listMovie').append(row);
         });
+        $("#listMovie .movieF").click(function() {
+            const aa = $(this).attr('id')
+            getmovieDetail(aa)
+            document.querySelector('#myNavigator_search').pushPage('views/detailmovie.html');
+        })
     });
-
+    document.addEventListener('init', function(event) {
+        var page = event.target;
+        if (page.id === 'search') {
+            $("ons-carousel-item .button").click(function() {
+                const category = $(this).attr('id')
+                getmovieCategory(category)
+            })
+        }
+    });
 });
 
+
+
+
 function Src() {
-
     const result = document.getElementById('Srcname').value;
-    console.log(result);
-
-    $('#list').empty();
+    $('#listMovie').empty();
     db.collection("movieDetail").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             const titleMovie = doc.data().title;
-
             if (titleMovie.indexOf(result) != -1) {
-                var row = `<ons-col onclick="openHome()">
-                <div class="user_thumbnail">
-                                <img src="${doc.data().img}" alt="" class="searchPoster"> 
-                            </div>
-                            <div class="username" id="user_2_name">${doc.data().title}</div>
-                </ons-col>
-
-               `;
-                $('#list').append(row);
+                var row = `<div class="areaMovie movieSrc" id="${doc.data().id}">
+                                <div class="user_thumbnail">
+                                    <img src="${doc.data().img}" alt="" class="searchPoster"> 
+                                </div>
+                            </div>`;
+                $('#listMovie').append(row);
             }
-
         });
+        $("#listMovie .movieSrc").click(function() {
+            const aa = $(this).attr('id')
+            getmovieDetail(aa)
+            document.querySelector('#myNavigator_search').pushPage('views/detailmovie.html');
+        })
     });
 
+}
+
+
+function getmovieCategory(Type) {
+    $('#listMovie').empty();
+    db.collection("movieDetail").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+            const typeMovie = doc.data().type;
+            if (typeMovie === Type) {
+                var row = `<div class="areaMovie movieCat" id="${doc.data().id}">
+                                <div class="user_thumbnail">
+                                    <img src="${doc.data().img}" alt="" class="searchPoster"> 
+                                </div>
+                            </div>`;
+                $('#listMovie').append(row);
+            }
+        });
+        $("#listMovie .movieCat").click(function() {
+            const aa = $(this).attr('id')
+            getmovieDetail(aa)
+            document.querySelector('#myNavigator_search').pushPage('views/detailmovie.html');
+        })
+    });
 }
